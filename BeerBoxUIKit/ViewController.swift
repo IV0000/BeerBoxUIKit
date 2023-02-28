2be43bfde3724129046f1eb3afadf77e17ae7d3b
Ivan Voloshchuk <keranamv@gmail.com>
2023-02-28 14:14:25 +0100

    Initial Commit

---
+++ BeerBoxUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerBoxUIKit
+//
+//  Created by Ivan Voloshchuk on 28/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
